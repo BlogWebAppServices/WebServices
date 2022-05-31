eb36b9393ba9c9e2c756c135e2c733d6e2f79014
altasegemen <49123561+altasegemen@users.noreply.github.com>
2022-05-31 18:17:49 +0300

    kullanıcı ekleme

---
+++ kulturDelisiWebAPI/kulturDelisiWebAPI/Migrations/20220531151302_usermodelcolumnsadded.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace kulturDelisiWebAPI.Migrations
+{
+    public partial class usermodelcolumnsadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
